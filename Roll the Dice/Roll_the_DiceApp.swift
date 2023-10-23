507dd9888eb132e80b7a20104792506e03b15b53
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2023-10-23 10:15:31 -0500

    Initial Commit

---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by Fabiola Rocha Marquez on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
